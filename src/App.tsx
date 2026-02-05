796d4b7b18637ca695eca8fa3f4107439785f525
jessicaorozco10 <jessicaorozcorodriguez@gmail.com>
2026-02-05 15:02:29 -0500

    updated homepage and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="App-header-panels">
+              <img src="/assets/panel1.png" alt="panel 1" width="350" height="350"/>
+              <img src="/assets/panel2.png" alt="panel 1" width="350" height="350"/>
+          </div>
+          <div className="App-header-panels">
+              <img src="/assets/panel3.png" alt="panel 1" width="350" height="350"/>
+              <img src="/assets/panel4.png" alt="panel 1" width="350" height="350"/>
+          </div>
+          <img src="/assets/the-wolfsonian.png" className="App-images" alt="fiu wolfsonian logo"/>
+        <h1 className="App-title">Harry Clarke Art Assistant</h1>
+          <div className="App-header-panels">
+              <img src="/assets/panel5.png" alt="panel 1" width="350" height="350"/>
+              <img src="/assets/panel6.png" alt="panel 1" width="350" height="350"/>
+          </div>
+          <div className="App-header-panels">
+              <img src="/assets/panel7.png" alt="panel 1" width="350" height="350"/>
+              <img src="/assets/panel8.png" alt="panel 1" width="350" height="350"/>
+          </div>
       </header>
     </div>
   );
