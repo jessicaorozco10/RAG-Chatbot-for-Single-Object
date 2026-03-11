7c1d0978ee19ff49c6a0eb3273eddd6ada50a37d
samantha1111-1111 <leazamasamantha1111@gmail.com>
2026-03-10 23:57:40 -0400

    chnage accessibility

--- harry-clarke-art-assistant/frontend/app/accessibility/page.tsx
+++ harry-clarke-art-assistant/frontend/app/accessibility/page.tsx
@@ -10,11 +10,28 @@ import ChatUI from "../component/chat";
 import { IoChatboxEllipses } from "react-icons/io5";
 
 export default function Chat() {
+  
   return (
     <div className="relative w-screen h-screen">
-      <Background />
-
-      <Glass>
+          <Background />
+    
+          <Glass>
+            
+    
+            {/*accessibility*/}
+            <span
+              style={{
+                position: 'absolute',
+                top: '20px',
+                left: '100px',
+                color: 'white',
+                fontSize: '30px',
+                fontWeight: 500,
+                zIndex: 20,
+              }}
+            >
+              Accessibility
+            </span>
         
 
         <div className="w-full h-full">
@@ -26,7 +43,14 @@ export default function Chat() {
             icon={<IoChatboxEllipses size={32}/>}
             style={{ position: 'absolute', top: '20px', left: '20px' }}
           />
-
+        <hr
+  className="border-white/50 absolute"
+  style={{
+    top: "80px",
+    left: "20px",
+    right: "20px",
+  }}
+/>
 
           
         </div>
