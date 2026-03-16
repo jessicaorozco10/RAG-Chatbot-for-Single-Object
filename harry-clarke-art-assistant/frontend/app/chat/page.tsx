fd0b296929ef54e476412d0fd8b8d85dff2f428f
samantha1111-1111 <leazamasamantha1111@gmail.com>
2026-03-16 17:38:00 -0400

    change color

--- harry-clarke-art-assistant/frontend/app/chat/page.tsx
+++ harry-clarke-art-assistant/frontend/app/chat/page.tsx
@@ -3,17 +3,15 @@ import { FaUniversalAccess } from "react-icons/fa";
 import { CiCamera } from "react-icons/ci";
 import { MdExplore } from "react-icons/md";
 import Button from "../component/button";
-import Background from "../component/background";
-import Glass from "../component/glass";
 import Image from "next/image";
 import ChatUI from "../component/chat";
 
 export default function Chat() {
   return (
-    <div className="relative w-screen h-screen">
-      <Background />
+    <div className="relative w-screen h-screen bg-black">
+      
 
-      <Glass>
+      
         {/*fiu image*/}
         <Image
           src="/assets/the-wolfsonian.png"
@@ -23,7 +21,7 @@ export default function Chat() {
           style={{
             position: 'absolute',
             top: '20px',
-            left: '20px',
+            left: '100px',
             zIndex: 20,
           }}
         />
@@ -33,7 +31,7 @@ export default function Chat() {
           style={{
             position: 'absolute',
             top: '60px',
-            left: '40px',
+            left: '120px',
             color: 'white',
             fontSize: '10px',
             fontWeight: 500,
@@ -58,28 +56,25 @@ export default function Chat() {
           <Button
             href="/panel"
             icon={<MdExplore size={32}/>}
-            style={{ position: 'absolute', top: '100px', right: '20px' }}
+            style={{ position: 'absolute', top: '20px', left: '20px' }}
           />
         </div>
         {/* Chat area */}
           <div
             className="absolute inset-x-0 bottom-0 z-10"
             style={{
-              paddingTop: "120px", // space below logo/text
+              paddingTop: "20px", // space below logo/text
               paddingLeft: "20px",
               paddingRight: "20px",
               paddingBottom: "20px",
-              height: "80vh",
+              height: "88vh",
             }}
           >
             <ChatUI
-              userBubbleClass="bg-white/80 text-black"
-              assistantBubbleClass="bg-blue-500/30 text-white"
-              inputClass="bg-white/80 text-black"
-              sendButtonClass="bg-white/80 text-black"
+              
             />
           </div>
-      </Glass>
+      
     </div>
   );
 }
